8fedf8028dba7bfcf0c191571ab5a93be0e172f8
valentina <valentinaa632@gmail.com>
2018-09-01 20:11:32 +1000

    Revert "initial commit"
    This reverts commit 1ff7c55fa115f327cb23a4dbc5136f0aa407fc39.

--- src/main/java/com/cts/dao/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cts.dao;
-
-import com.cts.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Integer>{
-}
--- src/main/java/com/cts/dao/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cts.dao;
-
-import com.cts.entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher,Integer>{
-}
